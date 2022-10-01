2fcc11defb1e6d4fe5835c1efc32cf857ab3e839
Bohdan Leonets <bohdan@mytowerapp.com>
2022-10-01 12:44:01 +0300

    Vite Config

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import './assets/main.css';
 
 // const autoImportedModules = import.meta.glob('./autoImports/*.js');
-const autoImportedModules = import.meta.globEager('./autoImports/*.js');
+// const autoImportedModules = import.meta.globEager('./autoImports/*.js');
 
 // for(const path in autoImportedModules) {
 //     console.log(autoImportedModules[path]());
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  build:{
+    assetsInlineLimit: 0,
+    cssCodeSplit: false,
+    sourcemap: false,
+  }
 });
