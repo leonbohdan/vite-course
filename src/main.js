32cafe6734de0540db7b49c64395c8c2e9ab52c8
Bohdan Leonets <bohdan@mytowerapp.com>
2022-10-01 12:15:57 +0300

    Glob Imports in Vite

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import './assets/main.css'
+import './assets/main.css';
+
+// const autoImportedModules = import.meta.glob('./autoImports/*.js');
+const autoImportedModules = import.meta.globEager('./autoImports/*.js');
+
+// for(const path in autoImportedModules) {
+//     console.log(autoImportedModules[path]());
+// }
 
 createApp(App).mount("#app");
